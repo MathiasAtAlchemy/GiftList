1eb0055b700af318151bf03d47115358a6375f7e
Mathias Perfumo <mathiasperfumo@Mathiass-MacBook-Pro.local>
2023-06-19 15:19:43 -0700

    Able to check whitelist

--- server/index.js
+++ server/index.js
@@ -1,29 +1,39 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
+const MerkleTree = require("../utils/MerkleTree");
+const niceList = require("../utils/niceList.json");
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
+const merkleTree = new MerkleTree(niceList);
+const merkleRoot = merkleTree.getRoot();
+console.log("merkleRoot", merkleRoot);
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "b3a5f33ce0cad3d25b517507b5a47bdef46d813b8a5cae085d30063e13cc33db";
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // TODO: prove that a name is in the list
+  //Name is in body
+  //body sends a proof
+  //verify that proof with .verify
+
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  console.log("root inside listen:", merkleRoot);
 });
